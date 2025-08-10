4f6dd951877c3253795af79d30352920ed0d8ca6
TAVERAT CHAIMAN <taverat@techtoptier.com>
2025-08-10 17:55:03 +0700

    set default lang to th

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -102,7 +102,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="th" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${notoSansThai.variable} antialiased`}>
         <Providers>{children}</Providers>
